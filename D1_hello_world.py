2c0d9dd72b60e4bfeb366362effe5cee3f530dc9
josephvalenti53 <89355014+josephvalenti53@users.noreply.github.com>
2023-08-25 13:19:52 -0500

    Add files via upload

---
+++ D1_hello_world.py
@@ -0,0 +1,63 @@
+print("Hello, Earth! It's me Joseph, I like legos, Splunk, photography, and home automation!  What about you?")
+
+C:\Users\18165\Documents\44671-80_Streaming
+
+CSIS 44-609: Practical Evaluation of Data - Complete
+CSIS 44-638: Financial Modeling and Decision Making for Information Technology (IT) - Complete
+CSIS 44-613: Data Visualization - Complete
+CSIS 44-620: Web Mining and Applied Natural Language Processing - Complete
+CSIS 44-608: Data Analytics Fundamentals - Complete
+CSIS 44-661: Database for Analytics - Complete
+CSIS 44-670: Applied Machine Learning - Complete
+CSIS 44-632: Business Intelligence (BI) and Analytics - Complete
+CSIS 44-671: Streaming Data – In progress
+CSIS 44-688: Data Analytics Capstone - Fall Block 2
+
+
+ covering large topics and scenarios where data is needed sooner rather than later to make . Like DNA evidence in a trial, it can be difficult debating such facts. 
+
+
+
+Submit
+Part 1 - Project 
+
+Paste a clickable link to your public GitHub repo: 
+About how long did you spend on class this module: 3 + 3 + 8
+In general, how did it go: Not where in CANVAS do I go for directions to complete the assignment?
+What was the most difficult part: Finding instructions
+What was most interesting:
+
+Part 2 - Self Assessment
+
+From the Module 1: Overview, paste the numbered list of objectives and assess your ability on each as "Highly proficient", "Proficient", or "Not Proficient":
+
+Part 3 - Self Review 
+
+Earn full  credit - do a self-review before you submit:
+
+Did you review your GitHub repo after commiting your files - is it well presented?
+Confirm the output of your util_about.py is shown in your repo:
+How many csv files do you have in your repo:
+Share a screen shot that shows your final code correctly running on your machine: 
+
+
+
+What am I supposed to accomplish.. there is not a   I feel there is no context to what I'm analyzing?
+
+After running 
+
+Where do 
+
+
+
+Part 2: 
+Define streaming data. (L01) - Highly Proficient
+Identify sources of streaming data. (L01) - Highly Proficient
+Compare streaming data sources and database systems. (L02) - Proficient
+Set up an environment for working with streaming data. (L02) - Proficient
+Execute Python examples (L02) - Proficient
+
+
+
+C:/Users/18165/AppData/Local/M   osoft/Winicrosoft/WindowsApps/python3.9.exe
+
